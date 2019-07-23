30a520aacc65669752cd4a152f71971da10e1c19
Carl Hinderer <carl.hinderer4@gmail.com>
2019-07-23 17:38:22 -0600

    Updated settings.py with Postgres database settings.

--- projects/coffeehouse-project/coffeehouse/settings.py
+++ projects/coffeehouse-project/coffeehouse/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'coffeehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coffeehouse',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangopw',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
