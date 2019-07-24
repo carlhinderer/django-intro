0aed08ac22a0d5204db55c11d72ca52565fb3265
Carl Hinderer <carl.hinderer4@gmail.com>
2019-07-23 22:53:41 -0600

    Added 'django.contrib.admindocs' app.

--- projects/coffeehouse-project/coffeehouse/urls.py
+++ projects/coffeehouse-project/coffeehouse/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
 from coffeehouse.about import views as about_views
 
 urlpatterns = [
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='homepage.html')),
     url(r'^about/', about_views.contact)
