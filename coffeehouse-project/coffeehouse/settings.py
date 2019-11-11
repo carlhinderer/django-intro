d7076be4dd4af8eb27619f18c15aa0af0e24ef7f
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-11 12:22:48 -0700

    Switched db back to postgres.

--- coffeehouse-project/coffeehouse/settings.py
+++ coffeehouse-project/coffeehouse/settings.py
@@ -79,15 +79,13 @@ WSGI_APPLICATION = 'coffeehouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'coffeehouse',
-        # 'USER': 'djangouser',
-        # 'PASSWORD': 'djangopw',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432'
-            }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'coffeehouse',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangopw',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
 }
 
 
