1aeff8cbd638fdf1205533737c836dccb7282003
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-13 11:52:34 -0700

    Added store model.

--- coffeehouse-project/coffeehouse/stores/models.py
+++ coffeehouse-project/coffeehouse/stores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return "%s (%s,%s)" % (self.name, self.city, self.state)
