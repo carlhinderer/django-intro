5e3701deb49a9b6c4c124cb82336ca6f4056fd0b
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-17 22:53:52 -0700

    Added sending of emails for sharing posts.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # Post views
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-        views.post_detail,
-        name='post_detail')
+         views.post_detail,
+         name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
 from .forms import EmailPostForm
@@ -18,19 +19,27 @@ def post_detail(request, year, month, day, post):
 
 
 def post_share(request, post_id):
-    # Retrieve post by id
     post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
 
     if request.method == 'POST':
-        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
             # Form fields passed validation
             cd = form.cleaned_data
-            # ... send email
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], 
+                                                                   cd['email'], 
+                                                                   post.title)
+            message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, 
+                                                                     post_url, 
+                                                                     cd['name'], 
+                                                                     cd['comments'])
+            send_mail(subject, message, 'admin@myblog.com', [cd['to']])
+            sent = True
     else:
         form = EmailPostForm()
-    return render(request, 'blog/post/share.html', {'post': post, 'form': form})
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
 
 
 class PostListView(ListView):
