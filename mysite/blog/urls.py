145751f575b9cbd06dbc98a2f8093ed63f85226c
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-17 21:49:46 -0700

    Made a class-based view for post detail.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'    # This lets us create a namespace for urls
 
 urlpatterns = [
     # Post views
-    path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
         views.post_detail,
         name='post_detail')
