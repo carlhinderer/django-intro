3319b24685daba0c1b359d11011a8c0c8541e940
Carl Hinderer <carl.hinderer4@gmail.com>
2019-11-17 19:57:27 -0700

    Added custom model manager for published posts.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+# blog/models.py
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self) \
+               .get_queryset(). \
+               filter(status='published')
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -20,6 +27,9 @@ class Post(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS_CHOICES,
                               default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
